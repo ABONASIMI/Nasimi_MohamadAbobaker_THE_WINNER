e0b189aa9b7f6a307dc8a367e03906c5a74e3b84
mnasimi <mnasimi>
2026-03-25 10:19:38 +0100

    just change the name and few code to go ahead and be in the center

--- src/agents/team6/agent6.py
+++ src/agents/team6/agent6.py
@@ -12,7 +12,7 @@ class Agent6(KartAgent):
         self.agent_positions = []
         self.obs = None
         self.isEnd = False
-        self.name = "Team6" # replace with your chosen name
+        self.name = "NASIMI Mohamad Abobaker" # replace with your chosen name
 
     def reset(self):
         self.obs, _ = self.env.reset()
@@ -22,15 +22,22 @@ class Agent6(KartAgent):
         return self.isEnd
 
     def choose_action(self, obs):
-        acceleration = random.random()
-        steering = random.random()
+        target = obs["paths_end"][0] #prendre des x,y,z de pist pour etre au centre 
+        steerx = target[0]  #prendre x pour entre en centre 
+        dist = obs["distance_down_track"]
+        acceleration = 0.90
+        if (abs(steerx) > 75 and dist > 25):
+            acceleration = 0.35
+
+
+
         action = {
             "acceleration": acceleration,
-            "steer": steering,
+            "steer": steerx,
             "brake": False, # bool(random.getrandbits(1)),
-            "drift": bool(random.getrandbits(1)),
-            "nitro": bool(random.getrandbits(1)),
-            "rescue":bool(random.getrandbits(1)),
-            "fire": bool(random.getrandbits(1)),
+            "drift": False,
+            "nitro": False,
+            "rescue": False,
+            "fire": False,
         }
         return action
